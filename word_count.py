dc239b75659a5229b9bf6d807f9981c59629bb6d
RajPrabhakar <rajprabhakar1804@gmail.com>
2018-07-09 18:24:58 +0530

    Specific keyword count done...

--- word_count.py
+++ word_count.py
@@ -6,7 +6,10 @@ target = [['method', 'program', 'feasibility', 'engineering', 'rahul', 'lax'],
           ['network', 'security', 'cryptography', 'encryption', 'decryption'],
           ['machine learning', 'nlp', 'neural', 'deep learning', 'robotics', 'automation'],
           ['database', 'query', 'records', 'tables', 'sql', 'oracle']]
-domain_total = [0, 0, 0, 0]
+domain_total = [[0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0]]
 domain = ['Mathematics', 'Communication', 'A.I', 'Database']
 word_count = 0
 fopen = open(file, "rb")
@@ -19,13 +22,16 @@ for para in docu.paragraphs:
         word = pattern.sub('', word)
         dom_count = 0
         for out_list in target:
+            key_count = 0
             for in_list in out_list:
                 if in_list == word:
-                    domain_total[dom_count]+=1
+                    domain_total[dom_count][key_count]+=1
+                key_count+=1
             dom_count+=1
         word_count+=1
 print(word_count)
 for i in range(4):
-    print(domain[i], " : ", domain_total[i])
+    for j in range(len(target[i])):
+        print(domain[i], " : ", target[i][j], " : ", domain_total[i][j])
     
 
